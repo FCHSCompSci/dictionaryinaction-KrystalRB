23cda4223d6df4d48d06a14514b2746553bdf9ba
Krystal Rain Bush <krystal.bush@stu.jefferson.kyschools.us>
2018-09-18 08:02:10 -0400

    added this feature

---
+++ gameProject.py
@@ -0,0 +1,11 @@
+resources = ["wood", "dirt", "sand", "sapling"]
+craftable_items1 = ["crafting table", "stick", "wooden pickaxe"]
+resources2 = ["cobblestone", "coal", "iron ore"]
+craftable_items2 = ["furnace", "torch"]
+
+def craft_one(crafting_table, stick, wooden_pickaxe):
+    create = {
+        'crafting table':crafting_table,
+        'stick':stick,
+        'wooden pickaxe':wooden_pickaxe,
+        }
