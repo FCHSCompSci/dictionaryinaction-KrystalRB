4cbd6ec7ba90329ac2e36dafa5046741368647e2
Krystal Rain Bush <krystal.bush@stu.jefferson.kyschools.us>
2018-09-18 08:33:55 -0400

    game project

--- gameProject.py
+++ gameProject.py
@@ -1,11 +1,26 @@
+import random
+
 resources = ["wood", "dirt", "sand", "sapling"]
 craftable_items1 = ["crafting table", "stick", "wooden pickaxe"]
 resources2 = ["cobblestone", "coal", "iron ore"]
 craftable_items2 = ["furnace", "torch"]
+inventory = []
 
 def craft_one(crafting_table, stick, wooden_pickaxe):
-    create = {
+    recipe = {
         'crafting table':crafting_table,
         'stick':stick,
         'wooden pickaxe':wooden_pickaxe,
         }
+
+recipe_two = {
+        'furnace':furnace,
+        'torch':torch,
+        }
+
+def get_resources():
+    resources.random()
+    inventory.append()
+
+#I don't understand when to use functions vs. when to use dictionaries vs. when to use dictionaries inside of functions
+
