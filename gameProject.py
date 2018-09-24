29e62206393946617cd0f7669443a4194c134e95
Krystal Rain Bush <krystal.bush@stu.jefferson.kyschools.us>
2018-09-24 08:17:47 -0400

    The first one

--- gameProject.py
+++ gameProject.py
@@ -1,26 +1,36 @@
+#Krystal Bush
+#9/24/18
+
 import random
 
 resources = ["wood", "dirt", "sand", "sapling"]
-craftable_items1 = ["crafting table", "stick", "wooden pickaxe"]
+craftable_items1 = ["crafting table", "stick"]
 resources2 = ["cobblestone", "coal", "iron ore"]
-craftable_items2 = ["furnace", "torch"]
+craftable_items2 = ["furnace", "torch", "wooden pickaxe"]
 inventory = []
 
 def craft_one(crafting_table, stick, wooden_pickaxe):
     recipe = {
         'crafting table':crafting_table,
         'stick':stick,
-        'wooden pickaxe':wooden_pickaxe,
         }
 
-recipe_two = {
+def craft_two(furnace, stick, wooden_pickaxe):
+    recipe_two = {
         'furnace':furnace,
         'torch':torch,
+        'wooden pickaxe':wooden_pickaxe,
         }
 
 def get_resources():
     resources.random()
     inventory.append()
 
+
+
 #I don't understand when to use functions vs. when to use dictionaries vs. when to use dictionaries inside of functions
+
+# I use functions anytime I want to DO something.
+
+
 
